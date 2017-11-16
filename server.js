66c3c5b919d5f56ae6ffd3c16cf14c23cbf7257d
krieg2 <aaronk1@yahoo.com>
2017-11-16 12:16:57 -0500

    style home page courtesy of startbootstrap.com

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-var app = express();
-var port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "/app/public")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require(path.join(__dirname, "/app/routing/apiRoutes.js"))(app);
+require(path.join(__dirname, "/app/routing/htmlRoutes.js"))(app);
 
 app.listen(port, () => console.log("Listening on port " + port));
